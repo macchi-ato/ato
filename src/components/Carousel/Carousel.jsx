0cabd095b0a89499ed5e5a02e5b77d3a08fcb418
macchi-ato <macchikay@gmail.com>
2026-02-17 10:42:10 -0500

    refactor: spacing

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,19 +1,19 @@
 import "./Carousel.css"
 
 export default function Carousel({ models, selectedId, onSelect }) {
-  return (
-    <div className="carousel">
-      <div className="carousel-container">
-        {models.map((model) => (
-          <button
-            key={model.id}
-            className={`carousel-item ${selectedId === model.id ? "active" : ""}`}
-            onClick={() => onSelect(model.id)}
-          >
-            {model.name}
-          </button>
-        ))}
-      </div>
-    </div>
-  )
+    return (
+        <div className="carousel">
+            <div className="carousel-container">
+            {models.map((model) => (
+                <button
+                    key={model.id}
+                    className={`carousel-item ${selectedId === model.id ? "active" : ""}`}
+                    onClick={() => onSelect(model.id)}
+                >
+                {model.name}
+                </button>
+            ))}
+            </div>
+        </div>
+    )
 }
