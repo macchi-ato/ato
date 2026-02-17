3ebe2ca35afdb76ede1288c2a9b8e79ff2606a52
macchi-ato <macchikay@gmail.com>
2026-02-17 09:52:49 -0500

    added carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import "./App.css"
+import { useState } from "react"
+import { models } from "./data/models"
 
+// Components
 import Experience from "./components/Experience/Experience.tsx"
+import Carousel from "./components/Carousel/Carousel.tsx"
 
 function App() {
+  const [selectedModelId, setSelectedModelId] = useState(models[0].id)
+  const currentModel = models.find(m => m.id === selectedModelId) || models[0]
+
   return (
-    <Experience />
+    <>
+      <Experience modelPath={currentModel.path} />
+      <Carousel 
+        models={models} 
+        selectedId={selectedModelId} 
+        onSelect={setSelectedModelId} 
+      />
+    </>
   )
 }
 
