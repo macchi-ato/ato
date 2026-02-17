529baf4aa65d1b1567547aca73144a272522438b
macchi-ato <macchikay@gmail.com>
2026-02-17 09:37:58 -0500

    models data

---
+++ src/data/models.ts
@@ -0,0 +1,13 @@
+export interface Model {
+    id: string
+    name: string
+    path: string
+}
+
+export const models: Model[] = [
+    {
+        id: "jiku",
+        name: "Jiku",
+        path: "/jiku.glb"
+    }
+]
