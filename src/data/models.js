d8ce97fca5b2bcc13a1094bfe77c76db6596e9d3
macchi-ato <macchikay@gmail.com>
2026-02-17 11:49:47 -0500

    added donut model

--- src/data/models.js
+++ src/data/models.js
@@ -3,5 +3,10 @@ export const models = [
         id: "jiku",
         name: "Jiku",
         path: "/jiku.glb"
+    },
+    {
+        id: "donut",
+        name: "donut",
+        path: "/donut.glb"
     }
 ]
