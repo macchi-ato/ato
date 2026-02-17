0918d7a5803a81636b4d3d617bae23e0d3fc2ab8
macchi-ato <macchikay@gmail.com>
2026-02-17 12:20:23 -0500

    refactor: moved carousel into the experience component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,9 @@
 import "./App.css"
-import { useState } from "react"
-import { models } from "./data/models"
-
-// Components
 import Experience from "./components/Experience/Experience.jsx"
-import Carousel from "./components/Carousel/Carousel.jsx"
 
 function App() {
-    const [selectedModelId, setSelectedModelId] = useState(models[0].id)
-    const currentModel = models.find(m => m.id === selectedModelId) || models[0]
-
     return (
-        <>
-            <Experience modelPath={currentModel.path} />
-            <Carousel 
-                models={models} 
-                selectedId={selectedModelId} 
-                onSelect={setSelectedModelId} 
-            />
-        </>
+        <Experience />
     )
 }
 
--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,21 +1,45 @@
 import "./Experience.css"
 import { Canvas } from "@react-three/fiber"
 import { OrbitControls, useGLTF } from "@react-three/drei"
+import { models } from "../../data/models.js"
+import { useState } from "react"
 
 function Model({ path }) {
     const { scene } = useGLTF(path)
     return <primitive object={scene} />
 }
 
-export default function Experience({ modelPath }) {
+export default function Experience() {
+    const [ modelPath, setModelPath ] = useState("/jiku.glb")
+
+    const setModel = (modelPath) => {
+        setModelPath(modelPath)
+    }
+
     return (
-        <div className="experience-container">
-            <Canvas camera={{ position: [0, 3, 8] }}>
-                <ambientLight intensity={0.5} />
-                <directionalLight position={[5, 5, 5]} />
-                <Model path={modelPath}  />
-                <OrbitControls />
-            </Canvas>
-        </div>
+        <>
+            <div className="experience-container">
+                <Canvas camera={{ position: [0, 3, 8] }}>
+                    <ambientLight intensity={0.5} />
+                    <directionalLight position={[5, 5, 5]} />
+                    <Model path={modelPath}/>
+                    <OrbitControls />
+                </Canvas>
+            </div>
+
+            <div className="carousel">
+                <div className="carousel-container">
+                {models.map((model) => (
+                    <button
+                        key={model.id}
+                        className={`carousel-item ${model.path === modelPath ? "active" : ""}`}
+                        onClick={() => setModel(model.path)}
+                    >
+                    {model.name}
+                    </button>
+                ))}
+                </div>
+            </div>     
+        </>
     )
 }
