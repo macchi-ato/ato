2fddbb56d7486054566175609d06d26a8e7fb66a
macchi-ato <macchikay@gmail.com>
2026-02-23 15:10:50 -0500

    camera properties adjustments

--- src/data/models.js
+++ src/data/models.js
@@ -3,30 +3,24 @@ export const models = [
         id: "jiku",
         name: "Jiku",
         path: "/jiku.glb",
-        camera: { position: [0, 4, 8], fov: 50 }
-    },
-    {
-        id: "donut",
-        name: "donut",
-        path: "/donut.glb",
-        camera: { position: [-4, 3, 5], fov: 50 }
+        camera: { position: [4, 5, 6], fov: 50 }
     },
     {
         id: "dragonHead",
-        name: "dragon head",
+        name: "Dragon Head",
         path: "/dragonHead.glb",
-        camera: { position: [0, 3, 12], fov: 50 }
+        camera: { position: [4, 3, 12], fov: 50 }
     },
     {
         id: "inkSerpent",
-        name: "ink serpent",
+        name: "Ink Serpent",
         path: "/inkSerpent.glb",
-        camera: { position: [-2, 6, 15], fov: 50 }
+        camera: { position: [-4, 6, 12], fov: 50 }
     },
     {
         id: "monkey",
-        name: "monkey",
+        name: "Monkey Tree",
         path: "/MonkeyBanana.glb",
-        camera: { position: [0, 10, 15], fov: 50 }
+        camera: { position: [0, 3, 8], fov: 50 }
     }
 ]
