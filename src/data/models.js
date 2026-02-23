4dec1d921c6c7f1c4b1dbc43ac3ccff2bc08dc8f
macchi-ato <macchikay@gmail.com>
2026-02-23 13:46:11 -0500

    added more models

--- src/data/models.js
+++ src/data/models.js
@@ -8,5 +8,20 @@ export const models = [
         id: "donut",
         name: "donut",
         path: "/donut.glb"
+    },
+    {
+        id: "dragonHead",
+        name: "dragon head",
+        path: "/dragonHead.glb"
+    },
+    {
+        id: "inkSerpent",
+        name: "ink serpent",
+        path: "/inkSerpent.glb"
+    },
+    {
+        id: "monkey",
+        name: "monkey",
+        path: "/MonkeyBanana.glb"
     }
 ]
